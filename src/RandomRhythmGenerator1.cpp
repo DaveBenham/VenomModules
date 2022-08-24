8b14e2acf0a978f78e23a2f38fee15d755ba4a7b
patheros <patheros@gmail.com>
2022-08-23 22:05:57 -0500

    Adding in a bunch more ports

--- src/RandomRhythmGenerator1.cpp
+++ src/RandomRhythmGenerator1.cpp
@@ -33,17 +33,24 @@ struct RandomRhythmGenerator1 : Module {
 		NEW_SEED_BUTTON_PARAM,
 		PATERN_LENGTH_PARAM,
 		ENUMS(RATE_PARAM, SLIDER_COUNT),
+		RESET_BUTTON_PARAM,
+		ENUMS(MUTE_CHANNEL_PARAM, SLIDER_COUNT),
 		PARAMS_LEN
 	};
 	enum InputId {
 		CLOCK_INPUT,
 		SEED_INPUT,	
 		RNG_OVERRIDE_INPUT,
+		RESET_TRIGGER_INPUT,
+		ENUMS(DENSITY_CHANNEL_INPUT, SLIDER_COUNT),
+		DENSITY_CHANNEL_POLY_INPUT,
+		NEW_SEED_TRIGGER_PARAM,
 		INPUTS_LEN
 	};
 	enum OutputId {
 		ENUMS(GATE_OUTPUT, SLIDER_COUNT),
 		SEED_OUTPUT,
+		GATE_POLY_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -61,26 +68,35 @@ struct RandomRhythmGenerator1 : Module {
 	int currentCycle;
 	rack::random::Xoroshiro128Plus rng;
 	bool clockHigh;
+	bool resetTrigHigh;
+	bool resetBtnHigh;
 	bool newSeedBtnHigh;
+	bool newSeedTrigHigh;
 	int gateHigh [SLIDER_COUNT];
 
 	RandomRhythmGenerator1() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 
 		configInput(CLOCK_INPUT,"Clock");
+		configInput(NEW_SEED_TRIGGER_PARAM,"New Seed");
+		configInput(RESET_TRIGGER_INPUT,"Reset");
 		configInput(SEED_INPUT,"Seed Input");
 		configInput(RNG_OVERRIDE_INPUT,"RNG Override");
 		configOutput(SEED_OUTPUT,"Seed Output");
 
 		configButton(NEW_SEED_BUTTON_PARAM, "New Seed");
+		configButton(RESET_BUTTON_PARAM,"Reset");
 		configParam(PATERN_LENGTH_PARAM, 1, MAX_PATTERN_LENGTH, 4, "Pattern Length");
 
 		for(int si = 0; si < SLIDER_COUNT; si++){
 			std::string si_s = std::to_string(si+1);
 			configSwitch(RATE_PARAM + si, 0, 7, si, "Rate " + si_s, SLIDER_LABELS);
 			configParam(DENSITY_PARAM + si, 0.f, 10.f, 0.f, "Density " + si_s, " V");
-			configOutput(GATE_OUTPUT + si, SLIDER_LABELS[si] + " gate");
+			configOutput(GATE_OUTPUT + si, "Gate " + si_s);
+			configInput(DENSITY_CHANNEL_INPUT + si, "Density CV" + si_s);
+			configSwitch(MUTE_CHANNEL_PARAM + si, 0, 1 , 0, "Mute " + si_s, {"Off","On"});
 		}
+		configInput(DENSITY_CHANNEL_POLY_INPUT,"Density Poly CV");
 
 		initalize();
 	}
@@ -95,6 +111,10 @@ struct RandomRhythmGenerator1 : Module {
 		currentCycle = 0;
 		rng = {};
 		clockHigh = false;
+		resetTrigHigh = false;
+		resetBtnHigh = false;
+		newSeedBtnHigh = false;
+		newSeedTrigHigh = false;
 		memset(gateHigh, 0, sizeof(gateHigh));
 		internalSeed = rack::random::uniform() * 10.f;
 		reseedRng();
@@ -117,9 +137,25 @@ struct RandomRhythmGenerator1 : Module {
 
 		//Clock Logic
 		bool clockEvent = schmittTrigger(clockHigh,inputs[CLOCK_INPUT].getVoltage());
-		bool newSeedEvent = buttonTrigger(newSeedBtnHigh,params[NEW_SEED_BUTTON_PARAM].getValue());
+		bool resetEvent = false;
+		if(schmittTrigger(resetTrigHigh,inputs[RESET_TRIGGER_INPUT].getVoltage())) resetEvent = true;
+		if(buttonTrigger(resetBtnHigh,params[RESET_BUTTON_PARAM].getValue())) resetEvent = true;
+
+		bool newSeedEvent = false;
+		if(schmittTrigger(newSeedBtnHigh,inputs[NEW_SEED_TRIGGER_PARAM].getVoltage())) newSeedEvent = true;
+		if(buttonTrigger(newSeedBtnHigh,params[NEW_SEED_BUTTON_PARAM].getValue())) newSeedEvent = true;
+
 		bool newCycle = false;
 		bool endOfCycle = false;
+
+		outputs[GATE_POLY_OUTPUT].setChannels(8);
+
+		if(resetEvent){
+			currentCycle = 0;
+			currentPulse = 0;
+			endOfCycle = true;
+			newSeedEvent = true;
+		}
 
 		if(newSeedEvent){
 			internalSeed = rack::random::uniform() * 10.f;
@@ -138,6 +174,7 @@ struct RandomRhythmGenerator1 : Module {
 					if(gateHigh[si] == 0){
 						//Gate Low
 						outputs[GATE_OUTPUT + si].setVoltage(0.f);
+						outputs[GATE_POLY_OUTPUT].setVoltage(0.f, si);
 					}
 				}
 
@@ -149,9 +186,12 @@ struct RandomRhythmGenerator1 : Module {
 					float rndFloat = ((rng() >> 32) * 2.32830629e-10f) * 10.f;
 					rndFloat = inputs[RNG_OVERRIDE_INPUT].getNormalVoltage(rndFloat, si);
 					float threshold = params[DENSITY_PARAM + si].getValue();
+					threshold = inputs[DENSITY_CHANNEL_INPUT + si].getNormalVoltage(threshold);
+					threshold = inputs[DENSITY_CHANNEL_POLY_INPUT].getNormalVoltage(threshold, si);
 
-					if(rndFloat < threshold){
+					if(rndFloat < threshold && params[MUTE_CHANNEL_PARAM + si].getValue() == 0){
 						outputs[GATE_OUTPUT + si].setVoltage(10.f);
+						outputs[GATE_POLY_OUTPUT].setVoltage(10.f, si);
 						gateHigh[si] = gateLength / 2;
 					}
 				}	
@@ -207,6 +247,14 @@ struct RandomRhythmGenerator1Widget : ModuleWidget {
 		}
 	};
 
+	struct MuteSwitch : app::SvgSwitch {
+		MuteSwitch() {
+			shadow->opacity = 0.0;
+			addFrame(Svg::load(asset::plugin(pluginInstance,"res/mute_0.svg")));
+			addFrame(Svg::load(asset::plugin(pluginInstance,"res/mute_1.svg")));
+		}
+	};
+
 	RandomRhythmGenerator1Widget(RandomRhythmGenerator1* module) {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Blank26hp.svg")));
@@ -230,14 +278,20 @@ struct RandomRhythmGenerator1Widget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(Vec(x,y), module, RandomRhythmGenerator1::RNG_OVERRIDE_INPUT));
 		y += dy;
 		addInput(createInputCentered<PJ301MPort>(Vec(x,y), module, RandomRhythmGenerator1::SEED_INPUT));
-		y += dy;
-		addOutput(createOutputCentered<PJ3410Port>(Vec(x,y), module, RandomRhythmGenerator1::SEED_OUTPUT));
+		addOutput(createOutputCentered<PJ3410Port>(Vec(x + dx,y), module, RandomRhythmGenerator1::SEED_OUTPUT));
 		y += dy;
 		addParam(createParamCentered<VCVButton>(Vec(x,y), module, RandomRhythmGenerator1::NEW_SEED_BUTTON_PARAM));
+		addInput(createInputCentered<PJ301MPort>(Vec(x + dx,y), module, RandomRhythmGenerator1::NEW_SEED_TRIGGER_PARAM));
+		y += dy;
+		addParam(createParamCentered<VCVButton>(Vec(x,y), module, RandomRhythmGenerator1::RESET_BUTTON_PARAM));
+		addInput(createInputCentered<PJ301MPort>(Vec(x + dx,y), module, RandomRhythmGenerator1::RESET_TRIGGER_INPUT));
 		y += dy;
 		addParam(createParamCentered<RotarySwitch<RoundSmallBlackKnob>>(Vec(x,y), module, RandomRhythmGenerator1::PATERN_LENGTH_PARAM));
-		
-		x = xStart + dx * 2;
+
+		x += dx;
+		y += dy;
+
+		x += dx * 2;
 		
 		for(int si = 0; si < SLIDER_COUNT; si++){
 			y = yStart;
@@ -245,12 +299,22 @@ struct RandomRhythmGenerator1Widget : ModuleWidget {
 			y += dy * 2;
 			addParam(createParamCentered<VCVSlider>(Vec(x,y), module, RandomRhythmGenerator1::DENSITY_PARAM + si));
 			y += dy * 2;
+			addInput(createInputCentered<PJ301MPort>(Vec(x,y), module, RandomRhythmGenerator1::DENSITY_CHANNEL_INPUT + si));
+			y += dy;
 			addOutput(createOutputCentered<PJ3410Port>(Vec(x,y), module, RandomRhythmGenerator1::GATE_OUTPUT + si));
+			y += dy;
+			addParam(createParamCentered<MuteSwitch>(Vec(x,y), module, RandomRhythmGenerator1::MUTE_CHANNEL_PARAM + si));			
+
 			x += dx;
 		}
 
-		x = xStart + dx * 1.75f;
-		y = yStart + dy * 6;
+		y = yStart + dy * 4;
+		addInput(createInputCentered<PJ301MPort>(Vec(x,y), module, RandomRhythmGenerator1::DENSITY_CHANNEL_POLY_INPUT));
+		y += dy;
+		addOutput(createOutputCentered<PJ3410Port>(Vec(x,y), module, RandomRhythmGenerator1::GATE_POLY_OUTPUT));
+
+		x = xStart + dx * 2.75f;
+		y = yStart + dy * 8;
 
 		for(int li = 0; li < MAX_PATTERN_LENGTH; li++){
 			addChild(createLightCentered<MediumLight<BlueLight>>(Vec(x,y), module, RandomRhythmGenerator1::PATERN_STEP_LIGHT + li));
