7ae5aa2fdf795beb89753b4dd3eeca1b73d5f5c8
Dave Benham <david.benham42@gmail.com>
2022-12-22 20:35:58 -0500

    Add manual trigger to Bernoulli Switch

--- src/BernoulliSwitch.cpp
+++ src/BernoulliSwitch.cpp
@@ -3,6 +3,7 @@
 struct BernoulliSwitch : Module {
   enum ParamId {
     PROB_PARAM,
+    TRIG_PARAM,
     MODE_PARAM,
     RISE_PARAM,
     FALL_PARAM,
@@ -27,6 +28,7 @@ struct BernoulliSwitch : Module {
   enum LightId {
     NO_SWAP_LIGHT,
     SWAP_LIGHT,
+    TRIG_LIGHT,
     LIGHTS_LEN
   };
   enum ProbMode {
@@ -44,6 +46,7 @@ struct BernoulliSwitch : Module {
   BernoulliSwitch() {
     config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
     configParam(PROB_PARAM, 0.f, 1.f, 0.5f, "Probability", "%", 0.f, 100.f, 0.f);
+    configButton(TRIG_PARAM, "Manual 10V Trigger");
     configSwitch(MODE_PARAM, 0, 2, 1, "Probability Mode", {"Toggle", "Swap", "Gate"});
     configParam(RISE_PARAM, -10.f, 10.f, 1.f, "Rise Threshold", " V");
     configParam(FALL_PARAM, -10.f, 10.f, 0.1f, "Fall Threshold", " V");
@@ -74,7 +77,8 @@ struct BernoulliSwitch : Module {
           offB = params[OFFSET_B_PARAM].getValue(),
           rise = params[RISE_PARAM].getValue(),
           fall = params[FALL_PARAM].getValue(),
-          probOff = params[PROB_PARAM].getValue();
+          probOff = params[PROB_PARAM].getValue(),
+          manual = params[TRIG_PARAM].getValue() > 0.f ? 10.f : 0.f;
     bool invTrig = rise < fall;
     int mode = static_cast<int>(params[MODE_PARAM].getValue());
     if (invTrig) {
@@ -104,7 +108,7 @@ struct BernoulliSwitch : Module {
     }
     for (int c=0; c<channels; c++){
       float prob = inputs[PROB_INPUT].getPolyVoltage(c)/10.f + probOff;
-      float trigIn = inputs[TRIG_INPUT].getPolyVoltage(c);
+      float trigIn = inputs[TRIG_INPUT].getPolyVoltage(c) + manual;
       float inA = inputs[A_INPUT].getNormalPolyVoltage(trigIn, c) * scaleA + offA;
       float inB = inputs[B_INPUT].getPolyVoltage(c) * scaleB + offB;
       if (trig[c].process(invTrig ? -trigIn : trigIn, fall, rise)){
@@ -132,6 +136,7 @@ struct BernoulliSwitch : Module {
           lights[SWAP_LIGHT].setBrightness(true);
         }
       }
+      lights[TRIG_LIGHT].setBrightness(manual);
       outputs[A_OUTPUT].setVoltage( swap[c] ? inB : inA, c);
       outputs[B_OUTPUT].setVoltage( swap[c] ? inA : inB, c);
     }
@@ -157,13 +162,14 @@ struct BernoulliSwitch : Module {
 struct BernoulliSwitchWidget : ModuleWidget {
   BernoulliSwitchWidget(BernoulliSwitch* module) {
     setModule(module);
-    setPanel(createPanel(asset::plugin(pluginInstance, "res/BernoulliSwitch4.svg")));
+    setPanel(createPanel(asset::plugin(pluginInstance, "res/BernoulliSwitch5.svg")));
     
     addChild(createLightCentered<SmallSimpleLight<YellowLight>>(mm2px(Vec(5.0, 18.75)), module, BernoulliSwitch::NO_SWAP_LIGHT));
     addChild(createLightCentered<SmallSimpleLight<YellowLight>>(mm2px(Vec(20.431, 18.75)), module, BernoulliSwitch::SWAP_LIGHT));
 
     addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(12.7155, 18.75)), module, BernoulliSwitch::PROB_PARAM));
-    addParam(createParam<CKSSThree>(mm2px(Vec(14.0, 25.0)), module, BernoulliSwitch::MODE_PARAM));
+    addParam(createLightParamCentered<VCVLightButton<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(6.5, 31.5)), module, BernoulliSwitch::TRIG_PARAM, BernoulliSwitch::TRIG_LIGHT));
+    addParam(createParam<CKSSThree>(mm2px(Vec(17.5, 25.0)), module, BernoulliSwitch::MODE_PARAM));
     addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(7.297, 43.87)), module, BernoulliSwitch::RISE_PARAM));
     addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(18.134, 43.87)), module, BernoulliSwitch::FALL_PARAM));
     addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(7.297, 58.3)), module, BernoulliSwitch::OFFSET_A_PARAM));
