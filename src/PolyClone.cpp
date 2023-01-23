cb599551b2d8e0e1a7a4b37bb4676f26bbc547ee
Dave Benham <david.benham42@gmail.com>
2023-01-23 09:30:30 -0500

    Cosmetic changes

--- src/PolyClone.cpp
+++ src/PolyClone.cpp
@@ -95,7 +95,7 @@ struct PolyCloneWidget : ModuleWidget {
     float y = RACK_GRID_WIDTH * 5.5f + 3;
     float dy = RACK_GRID_WIDTH * 2.f;
 
-    CountDisplay* countDisplay = createWidget<CountDisplay>(Vec(x-12, y-20));
+    CountDisplay* countDisplay = createWidget<CountDisplay>(Vec(x-12, y-23));
     countDisplay->module = module;
     addChild(countDisplay);
 
@@ -103,7 +103,7 @@ struct PolyCloneWidget : ModuleWidget {
     addParam(createParamCentered<RotarySwitch<RoundBlackKnob>>(Vec(x,y), module, PolyClone::CLONE_PARAM));
 
     y += dy*4.75f;
-    int ly = y+2;
+    int ly = y-2;
     for(int li = 0; li < 8; li++, ly-=dy/2.f){
       addChild(createLightCentered<MediumLight<YellowRedLight<>>>(Vec(x-dy/5.f,ly), module, (PolyClone::CHANNEL_LIGHTS+li)*2));
       addChild(createLightCentered<MediumLight<YellowRedLight<>>>(Vec(x+dy/5.f,ly), module, (PolyClone::CHANNEL_LIGHTS+li+8)*2));
