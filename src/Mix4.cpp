f69e69bbc774b6acd5625ac264344ed34701837b
Dave Benham <david.benham42@gmail.com>
2023-04-03 23:24:10 -0400

    Add new modes and clipping

--- src/Mix4.cpp
+++ src/Mix4.cpp
@@ -2,6 +2,8 @@
 // Licensed under GNU GPLv3
 
 #include "plugin.hpp"
+#include "dsp/math.hpp"
+#include "OversampleFilter.hpp"
 #include "ThemeStrings.hpp"
 
 #define MODULE_NAME Mix4
@@ -28,11 +30,12 @@ struct Mix4 : Module {
   };
 
   int mode = -1;
-  int clip = 0;
   bool connected[4] = {false, false, false, false};
   float normal = 0.f;
   float scale = 1.f;
   float offset = 0.f;
+  float oversample = 4.f;
+  OversampleFilter_4 outUpSample[4], outDownSample[4];
 
   #include "ThemeModVars.hpp"
 
@@ -43,13 +46,22 @@ struct Mix4 : Module {
       configInput(INPUTS+i, string::f("Channel %d", i + 1));
     }
     configParam(MIX_LEVEL_PARAM, 0.f, 2.f, 1.f, "Mix level", " dB", -10.f, 20.f);
-    configSwitch(MODE_PARAM, 0.f, 2.f, 0.f, "Level Mode", {"Audio dB", "CV%", "CV x10"});
-    configSwitch(CLIP_PARAM, 0.f, 2.f, 0.f, "Clipping", {"Off", "Soft audio clipping", "Hard CV clipping"});
+    configSwitch(MODE_PARAM, 0.f, 4.f, 0.f, "Level Mode", {"Audio dB", "Audio dB Poly Sum", "CV%", "CV x2", "CV x10"});
+    configSwitch(CLIP_PARAM, 0.f, 3.f, 0.f, "Clipping", {"Off", "Hard CV clipping", "Soft audio clipping", "Soft oversampled audio clipping"});
     configOutput(MIX_OUTPUT, "Mix");
+    initOversample();
+  }
+  
+  void initOversample(){
+    for (int i=0; i<4; i++){
+      outUpSample[i].setOversample(oversample);
+      outDownSample[i].setOversample(oversample);
+    }
   }
 
   void onReset(const ResetEvent& e) override {
     mode = -1;
+    initOversample();
     Module::onReset(e);
   }
 
@@ -59,36 +71,54 @@ struct Mix4 : Module {
         connected[1] != inputs[INPUTS + 1].isConnected() ||
         connected[2] != inputs[INPUTS + 2].isConnected() ||
         connected[3] != inputs[INPUTS + 3].isConnected()
-      ){
+    ){
       mode = static_cast<int>(params[MODE_PARAM].getValue());
       ParamQuantity* q;
       for (int i=0; i<4; i++) {
         connected[i] = inputs[INPUTS + i].isConnected();
         q = paramQuantities[LEVEL_PARAMS + i];
-        q->unit = mode == 0 ? " dB" : !connected[i] ? " V" : mode == 1 ? "%" : "x";
-        q->displayBase = mode == 0 ? -10.f : 0.f;
-        q->displayMultiplier = mode == 0 ? 20.f : (mode == 1 && connected[i]) ? 100.f : 10.f;
-        q->displayOffset = mode == 0 ? 0.f : (mode == 1 && connected[i]) ? -100.f : -10.f;
+        q->unit = mode <= 1 ? " dB" : !connected[i] ? " V" : mode == 2 ? "%" : "x";
+        q->displayBase = mode <= 1 ? -10.f : 0.f;
+        q->displayMultiplier = mode <= 1 ? 20.f : (mode == 2 && connected[i]) ? 100.f : (mode == 3 && connected[i]) ? 2.f : 10.f;
+        q->displayOffset = mode <= 1 ? 0.f : (mode == 1 && connected[i]) ? -100.f : (mode == 3 && connected[i]) ? -2.f : -10.f;
       }
       q = paramQuantities[MIX_LEVEL_PARAM];
-      q->unit = mode == 0 ? " dB" : mode == 1 ? "%" : "x";
-      q->displayBase = mode == 0 ? -10.f : 0.f;
-      q->displayMultiplier = mode == 0 ? 20.f : mode == 1 ? 100.f : 10.f;
-      q->displayOffset = mode == 0 ? 0.f : mode == 1 ? -100.f : -10.f;
-      q->defaultValue = mode == 0 ? 1.f : mode == 1 ? 2.f : 1.1f;
-      normal = mode == 0 ? 0.f : mode == 1 ? 10.f : 1.f;
-      scale = mode == 2 ? 10.f : 1.f;
-      offset = mode == 0 ? 0.f : -1.f;
+      q->unit = mode <= 1 ? " dB" : mode == 2 ? "%" : "x";
+      q->displayBase = mode <= 1 ? -10.f : 0.f;
+      q->displayMultiplier = mode <= 1 ? 20.f : mode == 2 ? 100.f : mode == 3 ? 2.f : 10.f;
+      q->displayOffset = mode <= 1 ? 0.f : mode == 2 ? -100.f : mode == 3 ? -2.f : -10.f;
+      q->defaultValue = mode <= 1 ? 1.f : mode == 2 ? 2.f : mode == 3 ? 1.5f : 1.1f;
+      normal = mode <= 1 ? 0.f : mode == 2 ? 10.f : mode == 3 ? 5.f : 1.f;
+      scale = mode == 4 ? 10.f : mode == 3 ? 2.f : 1.f;
+      offset = mode <= 1 ? 0.f : -1.f;
     }
+    int clip = static_cast<int>(params[CLIP_PARAM].getValue());
 
-    int channels = std::max({1, inputs[INPUTS].getChannels(), inputs[INPUTS+1].getChannels(), inputs[INPUTS+2].getChannels(), inputs[INPUTS+3].getChannels()});
-    simd::float_4 out = simd::float_4::zero();
+    int channels = mode == 1 ? 1 : std::max({1, inputs[INPUTS].getChannels(), inputs[INPUTS+1].getChannels(), inputs[INPUTS+2].getChannels(), inputs[INPUTS+3].getChannels()});
+    simd::float_4 out;
     for (int c=0; c<channels; c+=4){
-      out = inputs[INPUTS+0].getNormalPolyVoltageSimd<simd::float_4>(normal, c) * (params[LEVEL_PARAMS+0].getValue()+offset)*scale
+      out = mode == 1 ?
+            inputs[INPUTS+0].getVoltageSum() * (params[LEVEL_PARAMS+0].getValue()+offset)*scale
+          + inputs[INPUTS+1].getVoltageSum() * (params[LEVEL_PARAMS+1].getValue()+offset)*scale
+          + inputs[INPUTS+2].getVoltageSum() * (params[LEVEL_PARAMS+2].getValue()+offset)*scale
+          + inputs[INPUTS+3].getVoltageSum() * (params[LEVEL_PARAMS+3].getValue()+offset)*scale
+        :
+            inputs[INPUTS+0].getNormalPolyVoltageSimd<simd::float_4>(normal, c) * (params[LEVEL_PARAMS+0].getValue()+offset)*scale
           + inputs[INPUTS+1].getNormalPolyVoltageSimd<simd::float_4>(normal, c) * (params[LEVEL_PARAMS+1].getValue()+offset)*scale
           + inputs[INPUTS+2].getNormalPolyVoltageSimd<simd::float_4>(normal, c) * (params[LEVEL_PARAMS+2].getValue()+offset)*scale
           + inputs[INPUTS+3].getNormalPolyVoltageSimd<simd::float_4>(normal, c) * (params[LEVEL_PARAMS+3].getValue()+offset)*scale;
       out *= (params[MIX_LEVEL_PARAM].getValue()+offset)*scale;
+      if (clip == 1)
+        out = clamp(out, -10.f, 10.f);
+      if (clip == 2)
+        out = 10.f * tanh_rational5(out/9.5f);
+      if (clip == 3){
+        for (int i=0; i<oversample; i++){
+          out = outUpSample[c/4].process(i ? simd::float_4::zero() : out*oversample);
+          out = 10.f * tanh_rational5(out/9.5f);
+          out = outDownSample[c/4].process(out);
+        }
+      }
       outputs[MIX_OUTPUT].setVoltageSimd(out, c);
     }
     outputs[MIX_OUTPUT].setChannels(channels);
@@ -121,8 +151,10 @@ struct Mix4Widget : ModuleWidget {
   struct ModeSwitch : GlowingSvgSwitch {
     ModeSwitch() {
       shadow->opacity = 0.0;
+      addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallPinkButtonSwitch.svg")));
       addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallPurpleButtonSwitch.svg")));
       addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallGreenButtonSwitch.svg")));
+      addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallLightBlueButtonSwitch.svg")));
       addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallBlueButtonSwitch.svg")));
     }
   };
@@ -131,8 +163,9 @@ struct Mix4Widget : ModuleWidget {
     ClipSwitch() {
       shadow->opacity = 0.0;
       addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallOffButtonSwitch.svg")));
-      addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallYellowButtonSwitch.svg")));
       addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallWhiteButtonSwitch.svg")));
+      addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallYellowButtonSwitch.svg")));
+      addFrame(Svg::load(asset::plugin(pluginInstance,"res/smallOrangeButtonSwitch.svg")));
     }
   };
 
--- src/dsp/math.hpp
+++ src/dsp/math.hpp
@@ -1,72 +1,93 @@
 #include <array>
 
+// Fifth-order rational approximation to tanh
+inline float tanh_rational5(float x) {
+  if (x <= -3.0f) {
+    return -1.0f;
+  }
+  if (x >= 3.0f) {
+    return 1.0f;
+  }
+  float x2 = x * x;
+  return x * (27.0f + x2) / (27.0f + 9.0f * x2);
+}
+
+// Fifth-order rational approximation to tanh (simd)
+template <typename T>
+T tanh_rational5(T x) {
+  T overmask = (x >= static_cast<T>(3.0));
+  T undermask = (x <= static_cast<T>(-3.0));
+  T x2 = x * x;
+  x = x * (static_cast<T>(27.0) + x2) / (static_cast<T>(27.0) + static_cast<T>(9.0) * x2);
+  return rack::simd::ifelse(overmask, static_cast<T>(1.0), rack::simd::ifelse(undermask, static_cast<T>(-1.0), x));
+}
+
 
 using std::array;
-
 
 // four point, fourth-order b-spline polyblep, from:
 // Välimäki, Pekonen, Nam. "Perceptually informed synthesis of bandlimited
 // classical waveforms using integrated polynomial interpolation"
 inline void polyblep4(array<float, 4> &buffer, float d, float u) {
-	if (d > 1.0f) {
-		d = 1.0f;
-	}
-	else if (d < 0.0f) {
-		d = 0.0f;
-	}
+  if (d > 1.0f) {
+    d = 1.0f;
+  }
+  else if (d < 0.0f) {
+    d = 0.0f;
+  }
 
-	float d2 = d * d;
-	float d3 = d2 * d;
-	float d4 = d3 * d;
-	float dd3 = 0.16667 * (d + d3);
-	float cd2 = 0.041667 + 0.25 * d2;
-	float d4_1 = 0.041667 * d4;
+  float d2 = d * d;
+  float d3 = d2 * d;
+  float d4 = d3 * d;
+  float dd3 = 0.16667 * (d + d3);
+  float cd2 = 0.041667 + 0.25 * d2;
+  float d4_1 = 0.041667 * d4;
 
-	buffer[3] += u * (d4_1);
-	buffer[2] += u * (cd2 + dd3 - 0.125 * d4);
-	buffer[1] += u * (-0.5 + 0.66667 * d - 0.33333 * d3 + 0.125 * d4);
-	buffer[0] += u * (-cd2 + dd3 - d4_1);
+  buffer[3] += u * (d4_1);
+  buffer[2] += u * (cd2 + dd3 - 0.125 * d4);
+  buffer[1] += u * (-0.5 + 0.66667 * d - 0.33333 * d3 + 0.125 * d4);
+  buffer[0] += u * (-cd2 + dd3 - d4_1);
 }
 
 
 // four point, fourth-order b-spline polyblamp, from:
 // Esqueda, Välimäki, Bilbao. "Rounding Corners with BLAMP".
 inline void polyblamp4(array<float, 4> &buffer, float d, float u) {
-	if (d > 1.0f) {
-		d = 1.0f;
-	}
-	else if (d < 0.0f) {
-		d = 0.0f;
-	}
+  if (d > 1.0f) {
+    d = 1.0f;
+  }
+  else if (d < 0.0f) {
+    d = 0.0f;
+  }
 
-	float d2 = d * d;
-	float d3 = d2 * d;
-	float d4 = d3 * d;
-	float d5 = d4 * d;
-	float d5_1 = 0.0083333 * d5;
-	float d5_2 = 0.025 * d5;
+  float d2 = d * d;
+  float d3 = d2 * d;
+  float d4 = d3 * d;
+  float d5 = d4 * d;
+  float d5_1 = 0.0083333 * d5;
+  float d5_2 = 0.025 * d5;
 
-	buffer[3] += u * (d5_1);
-	buffer[2] += u * (0.0083333 + 0.083333 * (d2 + d3) + 0.041667 * (d + d4) - d5_2);
-	buffer[1] += u * (0.23333 - 0.5 * d + 0.33333 * d2 - 0.083333 * d4 + d5_2);
-	buffer[0] += u * (0.0083333 + 0.041667 * (d4 - d) + 0.083333 * (d2 - d3) - d5_1);
+  buffer[3] += u * (d5_1);
+  buffer[2] += u * (0.0083333 + 0.083333 * (d2 + d3) + 0.041667 * (d + d4) - d5_2);
+  buffer[1] += u * (0.23333 - 0.5 * d + 0.33333 * d2 - 0.083333 * d4 + d5_2);
+  buffer[0] += u * (0.0083333 + 0.041667 * (d4 - d) + 0.083333 * (d2 - d3) - d5_1);
 }
 
 
 // fast sine calculation. modified from the Reaktor 6 core library.
 // takes a [0, 1] range and folds it to a triangle on a [0, 0.5] range.
 inline float sin_01(float t) {
-	if (t > 1.0f) {
-		t = 1.0f;
-	}
-	else if (t > 0.5) {
-		t = 1.0f - t;
-	}
-	else if (t < 0.0f) {
-		t = 0.0f;
-	}
-	t = 2.0f * t - 0.5f;
-	float t2 = t * t;
-	t = (((-0.540347 * t2 + 2.53566) * t2 - 5.16651) * t2 + 3.14159) * t;
-	return t;
+  if (t > 1.0f) {
+    t = 1.0f;
+  }
+  else if (t > 0.5) {
+    t = 1.0f - t;
+  }
+  else if (t < 0.0f) {
+    t = 0.0f;
+  }
+  t = 2.0f * t - 0.5f;
+  float t2 = t * t;
+  t = (((-0.540347 * t2 + 2.53566) * t2 - 5.16651) * t2 + 3.14159) * t;
+  return t;
 }
