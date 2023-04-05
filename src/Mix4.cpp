b2a50fb914c5514647ba0e93df5a9f10986c669a
Dave Benham <david.benham42@gmail.com>
2023-04-05 04:17:45 -0400

    Update Mix4.cpp
    Add parameter locks

--- src/Mix4.cpp
+++ src/Mix4.cpp
@@ -39,19 +39,74 @@ struct Mix4 : Module {
 
   #include "ThemeModVars.hpp"
 
+  void appendMenu(Menu* menu, int parmId) {
+    menu->addChild(new MenuSeparator);
+    menu->addChild(createBoolMenuItem("Lock parameter", "",
+      [=]() {
+        return paramLocks[parmId].locked;
+      },
+      [=](bool val){
+        setLock(val, parmId);
+      }
+    ));
+  }
+
+  struct ParamLock {
+    bool locked;
+    bool initLocked;
+    float min, max, dflt;
+    ParamLock(){
+      locked = false;
+      initLocked = false;
+    }
+  };
+
+  void setLock(bool val, int id) {
+    if (paramLocks[id].locked != val){
+      paramLocks[id].locked = val;
+      ParamQuantity* q = paramQuantities[id];
+      if (val){
+        paramLocks[id].min = q->minValue;
+        paramLocks[id].max = q->maxValue;
+        paramLocks[id].dflt = q->defaultValue;
+        q->name += " (locked)";
+        q->minValue = q->maxValue = q->defaultValue = q->getValue();
+      }
+      else {
+        q->name.erase(q->name.length()-9);
+        q->minValue = paramLocks[id].min;
+        q->maxValue = paramLocks[id].max;
+        q->defaultValue = paramLocks[id].dflt;
+      }
+    }
+  }
+
+  void setLockAll(bool val){
+    for (int i=0; i<PARAMS_LEN; i++)
+      setLock(val, i);
+  }
+
+  ParamLock paramLocks[PARAMS_LEN];
+  bool paramLocksInitialized = false;
+
   Mix4() {
+    struct FixedSwitchQuantity : SwitchQuantity {
+      std::string getDisplayValueString() override {
+        return labels[getValue()];
+      }
+    };
     config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
     for (int i=0; i < 4; i++){
       configParam(LEVEL_PARAMS+i, 0.f, 2.f, 1.f, string::f("Channel %d level", i + 1), " dB", -10.f, 20.f);
       configInput(INPUTS+i, string::f("Channel %d", i + 1));
     }
     configParam(MIX_LEVEL_PARAM, 0.f, 2.f, 1.f, "Mix level", " dB", -10.f, 20.f);
-    configSwitch(MODE_PARAM, 0.f, 4.f, 0.f, "Level Mode", {"Audio dB", "Audio dB Poly Sum", "CV%", "CV x2", "CV x10"});
-    configSwitch(CLIP_PARAM, 0.f, 3.f, 0.f, "Clipping", {"Off", "Hard CV clipping", "Soft audio clipping", "Soft oversampled audio clipping"});
+    configSwitch<FixedSwitchQuantity>(MODE_PARAM, 0.f, 4.f, 0.f, "Level Mode", {"Unipolar audio dB", "Unipolar audio dB poly sum", "Bipolar CV%", "Bipolar CV x2", "Bipolar CV x10"});
+    configSwitch<FixedSwitchQuantity>(CLIP_PARAM, 0.f, 3.f, 0.f, "Clipping", {"Off", "Hard CV clipping", "Soft audio clipping", "Soft oversampled audio clipping"});
     configOutput(MIX_OUTPUT, "Mix");
     initOversample();
   }
-  
+
   void initOversample(){
     for (int i=0; i<4; i++){
       outUpSample[i].setOversample(oversample);
@@ -66,6 +121,12 @@ struct Mix4 : Module {
   }
 
   void process(const ProcessArgs& args) override {
+    if (!paramLocksInitialized){
+      paramLocksInitialized = true;
+      for (int i=0; i<PARAMS_LEN; i++){
+        setLock(paramLocks[i].initLocked, i);
+      }
+    }
     if( static_cast<int>(params[MODE_PARAM].getValue()) != mode ||
         connected[0] != inputs[INPUTS + 0].isConnected() ||
         connected[1] != inputs[INPUTS + 1].isConnected() ||
@@ -126,11 +187,21 @@ struct Mix4 : Module {
 
   json_t* dataToJson() override {
     json_t* rootJ = json_object();
+    for (int i=0; i<PARAMS_LEN; i++){
+      std::string nm = "paramLock"+std::to_string(i);
+      json_object_set_new(rootJ, nm.c_str(), json_boolean(paramLocks[i].locked));
+    }
     #include "ThemeToJson.hpp"
     return rootJ;
   }
 
   void dataFromJson(json_t* rootJ) override {
+    json_t* val;
+    for (int i=0; i<PARAMS_LEN; i++){
+      std::string nm = "paramLock"+std::to_string(i);
+      if ((val = json_object_get(rootJ, nm.c_str())))
+        paramLocks[i].initLocked = json_boolean_value(val);
+    }
     #include "ThemeFromJson.hpp"
   }
 };
@@ -145,6 +216,11 @@ struct Mix4Widget : ModuleWidget {
         }
       }
       app::SvgSwitch::drawLayer(args, layer);
+    }
+
+    void appendContextMenu(Menu* menu) override {
+      if (module)
+        dynamic_cast<Mix4*>(this->module)->appendMenu(menu, this->paramId);
     }
   };
 
@@ -169,15 +245,29 @@ struct Mix4Widget : ModuleWidget {
     }
   };
 
+  struct RoundSmallBlackKnobCustom : RoundSmallBlackKnob {
+    void appendContextMenu(Menu* menu) override {
+      if (module)
+        dynamic_cast<Mix4*>(this->module)->appendMenu(menu, this->paramId);
+    }
+  };
+
+  struct RoundBlackKnobCustom : RoundBlackKnob {
+    void appendContextMenu(Menu* menu) override {
+      if (module)
+        dynamic_cast<Mix4*>(this->module)->appendMenu(menu, this->paramId);
+    }
+  };
+
   Mix4Widget(Mix4* module) {
     setModule(module);
     setPanel(createPanel(asset::plugin(pluginInstance, faceplatePath(moduleName, module ? module->currentThemeStr() : themes[getDefaultTheme()]))));
 
-    addParam(createParamCentered<RoundSmallBlackKnob>(Vec(22.337, 34.295), module, Mix4::LEVEL_PARAMS+0));
-    addParam(createParamCentered<RoundSmallBlackKnob>(Vec(22.337, 66.535), module, Mix4::LEVEL_PARAMS+1));
-    addParam(createParamCentered<RoundSmallBlackKnob>(Vec(22.337, 98.775), module, Mix4::LEVEL_PARAMS+2));
-    addParam(createParamCentered<RoundSmallBlackKnob>(Vec(22.337,131.014), module, Mix4::LEVEL_PARAMS+3));
-    addParam(createParamCentered<RoundBlackKnob>(Vec(22.337,168.254), module, Mix4::MIX_LEVEL_PARAM));
+    addParam(createParamCentered<RoundSmallBlackKnobCustom>(Vec(22.337, 34.295), module, Mix4::LEVEL_PARAMS+0));
+    addParam(createParamCentered<RoundSmallBlackKnobCustom>(Vec(22.337, 66.535), module, Mix4::LEVEL_PARAMS+1));
+    addParam(createParamCentered<RoundSmallBlackKnobCustom>(Vec(22.337, 98.775), module, Mix4::LEVEL_PARAMS+2));
+    addParam(createParamCentered<RoundSmallBlackKnobCustom>(Vec(22.337,131.014), module, Mix4::LEVEL_PARAMS+3));
+    addParam(createParamCentered<RoundBlackKnobCustom>(Vec(22.337,168.254), module, Mix4::MIX_LEVEL_PARAM));
     addParam(createParamCentered<ModeSwitch>(Vec(36.674,50.415), module, Mix4::MODE_PARAM));
     addParam(createParamCentered<ClipSwitch>(Vec(36.674,82.655), module, Mix4::CLIP_PARAM));
 
@@ -191,6 +281,17 @@ struct Mix4Widget : ModuleWidget {
   void appendContextMenu(Menu* menu) override {
     Mix4* module = dynamic_cast<Mix4*>(this->module);
     assert(module);
+    menu->addChild(new MenuSeparator);
+    menu->addChild(createMenuItem("Lock all parameters", "",
+      [=]() {
+        module->setLockAll(true);
+      }
+    ));
+    menu->addChild(createMenuItem("Unlock all parameters", "",
+      [=]() {
+        module->setLockAll(false);
+      }
+    ));
     #include "ThemeMenu.hpp"
   }
 
