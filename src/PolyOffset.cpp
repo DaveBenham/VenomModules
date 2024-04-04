b7e766c665eb43803154ca469393f1243ff2b7b8
Dave Benham <david.benham42@gmail.com>
2024-04-04 08:27:16 -0400

    clone mono, PolyOffset quant & unit options, PolyOffset channels option

--- src/PolyOffset.cpp
+++ src/PolyOffset.cpp
@@ -32,6 +32,8 @@ struct PolyOffset : VenomModule {
     {2.f,-1.f,0.5f}, {4.f,-2.f,0.5f}, {10.f,-5.f,0.5f}, {20.f,-10.f,0.5f}
   };
   int rangeId = 7;
+  int quant = 0;
+  int unit = 0;
   int channels = 0;
   
   void setRange(int val) {
@@ -45,11 +47,39 @@ struct PolyOffset : VenomModule {
     }
   }
   
+  void setUnit(int val) {
+    unit = val;
+    std::string unitStr = unit ? " \u00A2" : " V";
+    for (int i=0; i<16; i++){
+      paramQuantities[OFFSET_PARAM+i]->unit = unitStr;
+    }
+  }
+  
+  struct OffsetQuantity : ParamQuantity {
+    float getDisplayValue() override {
+      PolyOffset* mod = reinterpret_cast<PolyOffset*>(module);
+      float val = ParamQuantity::getDisplayValue(); // Continuous
+      switch (mod->quant) {
+        case 1: // Integer
+          val = round(val);
+          break;
+        case 2: // Semitone
+          val = round(val*12.f)/12.f;
+      }
+      if (mod->unit) val *= 1200.f;
+      return val;
+    }
+    void setDisplayValue(float val) override {
+      PolyOffset* mod = reinterpret_cast<PolyOffset*>(module);
+      ParamQuantity::setDisplayValue( mod->unit ? val/1200.f : val);
+    }
+  };
+
   PolyOffset() {
     venomConfig(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
     for (int i=0; i<16; i++) {
       std::string nm = "Offset " + std::to_string(i+1);
-      configParam(OFFSET_PARAM+i, 0.f, 1.f, 0.5f, nm, " V", 0.f, 20.f, -10.f);
+      configParam<OffsetQuantity>(OFFSET_PARAM+i, 0.f, 1.f, 0.5f, nm, " V", 0.f, 20.f, -10.f);
     }
     configInput(POLY_INPUT,"Poly");
     configOutput(POLY_OUTPUT,"Poly");
@@ -60,11 +90,21 @@ struct PolyOffset : VenomModule {
     VenomModule::process(args);
     using float_4 = simd::float_4;
     float_4 offset{};
+    float_4 voltage{};
     int cnt = channels ? channels : inputs[POLY_INPUT].getChannels();
     for (int i=0; i<cnt; i+=4){
       for (int j=0; j<4; j++)
         offset[j] = params[OFFSET_PARAM+i+j].getValue();
-      outputs[POLY_OUTPUT].setVoltageSimd( inputs[POLY_INPUT].getVoltageSimd<float_4>(i) + (offset * ranges[rangeId].scale) + ranges[rangeId].offset, i);
+      voltage = inputs[POLY_INPUT].getPolyVoltageSimd<float_4>(i) + (offset * ranges[rangeId].scale) + ranges[rangeId].offset;
+      switch (quant) {
+        case 1: // Integer
+          voltage = simd::round(voltage);
+          break;
+        case 2: // Semitone
+          voltage = simd::round(voltage*12.f)/12.f;
+          break;
+      }
+      outputs[POLY_OUTPUT].setVoltageSimd(voltage, i);
     }
     outputs[POLY_OUTPUT].setChannels(cnt);
   }
@@ -72,6 +112,8 @@ struct PolyOffset : VenomModule {
   json_t* dataToJson() override {
     json_t* rootJ = VenomModule::dataToJson();
     json_object_set_new(rootJ, "rangeId", json_integer(rangeId));
+    json_object_set_new(rootJ, "quant", json_integer(quant));
+    json_object_set_new(rootJ, "unit", json_integer(unit));
     json_object_set_new(rootJ, "channels", json_integer(channels));
     return rootJ;
   }
@@ -81,6 +123,10 @@ struct PolyOffset : VenomModule {
     json_t* val;
     if ((val = json_object_get(rootJ, "rangeId")))
       setRange(json_integer_value(val));
+    if ((val = json_object_get(rootJ, "quant")))
+      quant = json_integer_value(val);
+    if ((val = json_object_get(rootJ, "unit")))
+      setUnit(json_integer_value(val));
     if ((val = json_object_get(rootJ, "channels")))
       channels = json_integer_value(val);
   }
@@ -135,6 +181,21 @@ struct PolyOffsetWidget : VenomWidget {
       },
       [=](int rangeId) {
         module->setRange(rangeId);
+      }
+    ));
+    menu->addChild(createIndexPtrSubmenuItem(
+      "Quantize",
+      {"Off (Continuous)","Integers (Octaves)","1/12V (Semitones)"},
+      &module->quant
+    ));
+    menu->addChild(createIndexSubmenuItem(
+      "Display unit", 
+      {"Volts (V)","Cents (\u00A2)"},
+      [=]() {
+        return module->unit;
+      },
+      [=](int val) {
+        module->setUnit(val);
       }
     ));
     menu->addChild(createIndexPtrSubmenuItem(
