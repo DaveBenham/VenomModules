c81dfa8f9b691dcdc7580afc94925e1e33116a37
Dave Benham <david.benham42@gmail.com>
2024-10-16 12:13:51 -0400

    First cut with full functionality

--- src/MousePad.cpp
+++ src/MousePad.cpp
@@ -6,6 +6,10 @@
 #define LIGHT_ON 1.f
 #define LIGHT_OFF 0.02f
 
+#define SHIFT_PRESS 1
+#define CTRL_PRESS 2
+#define ALT_PRESS 4
+
 struct MousePad : VenomModule {
 
   enum ParamId {
@@ -47,6 +51,7 @@ struct MousePad : VenomModule {
 //  math::Vec mousePos{};
   float x=0.f;
   float y=0.f;
+  float gate=0.f;
   
   MousePad() {
     venomConfig(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
@@ -56,8 +61,8 @@ struct MousePad : VenomModule {
     configSwitch<FixedSwitchQuantity>(CTRL_PARAM, 0.f, 1.f, 0.f, "Control activation", {"Off", "On"});
     configSwitch<FixedSwitchQuantity>(ALT_PARAM, 0.f, 1.f, 0.f, "Alt activation", {"Off", "On"});
 
-    configParam(XSCALE_PARAM, -4.f, 4.f, 1.f, "X scale");
-    configParam(YSCALE_PARAM, -4.f, 4.f, 1.f, "Y scale");
+    configParam(XSCALE_PARAM, -2.f, 2.f, 1.f, "X scale");
+    configParam(YSCALE_PARAM, -2.f, 2.f, 1.f, "Y scale");
     configParam(XORIGIN_PARAM, 0.f, 1.f, 0.5f, "X origin", "%", 0, 100, 0);
     configParam(YORIGIN_PARAM, 0.f, 1.f, 0.5f, "Y origin", "%", 0, 100, 0);
 
@@ -73,6 +78,7 @@ struct MousePad : VenomModule {
 
   void process(const ProcessArgs& args) override {
     VenomModule::process(args);
+    outputs[GATE_OUTPUT].setVoltage(gate);
     outputs[X_OUTPUT].setVoltage(x);
     outputs[Y_OUTPUT].setVoltage(y);
   }
@@ -80,10 +86,19 @@ struct MousePad : VenomModule {
 };
 
 struct MousePadWidget : VenomWidget {
-
+  
+  MousePad* mod = NULL;
+  bool activateState = false;
+  bool active = false;
+  float xRelativeOrigin = 0.f;
+  float yRelativeOrigin = 0.f;
+  float xSave = 0.f;
+  float ySave = 0.f;
+  
   MousePadWidget(MousePad* module) {
     setModule(module);
     setVenomPanel("MousePad");
+    mod = module;
 
     addParam(createLockableLightParamCentered<VCVLightButtonLatchLockable<MediumSimpleLight<WhiteLight>>>(Vec(12.f,59.f), module, MousePad::SHIFT_PARAM, MousePad::SHIFT_LIGHT));
     addParam(createLockableLightParamCentered<VCVLightButtonLatchLockable<MediumSimpleLight<WhiteLight>>>(Vec(33.f,59.f), module, MousePad::TOG_PARAM, MousePad::TOG_LIGHT));
@@ -106,11 +121,8 @@ struct MousePadWidget : VenomWidget {
   }
   
   void step() override {
-    if (module){
-      MousePad* mod = static_cast<MousePad*>(module);
-      mod->x = clamp(APP->scene->getMousePos().x/APP->scene->box.size.x,0.f,1.f)*10.f;
-      mod->y = (1-clamp(APP->scene->getMousePos().y/APP->scene->box.size.y,0.f,1.f))*10.f;
-      
+    VenomWidget::step();
+    if (mod){
       mod->lights[MousePad::SHIFT_LIGHT].setBrightness(mod->params[MousePad::SHIFT_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
       mod->lights[MousePad::TOG_LIGHT].setBrightness(mod->params[MousePad::TOG_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
       mod->lights[MousePad::CTRL_LIGHT].setBrightness(mod->params[MousePad::CTRL_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
@@ -119,8 +131,60 @@ struct MousePadWidget : VenomWidget {
       mod->lights[MousePad::YABS_LIGHT].setBrightness(mod->params[MousePad::YABS_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
       mod->lights[MousePad::XRETURN_LIGHT].setBrightness(mod->params[MousePad::XRETURN_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
       mod->lights[MousePad::YRETURN_LIGHT].setBrightness(mod->params[MousePad::YRETURN_PARAM].getValue() ? LIGHT_ON : LIGHT_OFF);
+
+      int activateMask = (mod->params[MousePad::SHIFT_PARAM].getValue() ? SHIFT_PRESS : 0)
+                       + (mod->params[MousePad::CTRL_PARAM].getValue() ? CTRL_PRESS : 0)
+                       + (mod->params[MousePad::ALT_PARAM].getValue() ? ALT_PRESS : 0);
+      if (!activateMask){
+        active = false;
+        activateState = false;
+        mod->x = 0.f;
+        mod->y = 0.f;
+        mod->gate = 0.f;
+        xSave = 0.f;
+        ySave = 0.f;
+        return;
+      }
+      int keys = (glfwGetKey(APP->window->win, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(APP->window->win, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS ? SHIFT_PRESS : 0)
+               + (glfwGetKey(APP->window->win, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS || glfwGetKey(APP->window->win, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS ? CTRL_PRESS : 0)
+               + (glfwGetKey(APP->window->win, GLFW_KEY_LEFT_ALT) == GLFW_PRESS || glfwGetKey(APP->window->win, GLFW_KEY_RIGHT_ALT) == GLFW_PRESS ? ALT_PRESS : 0);
+      bool activate = (keys == activateMask);
+      float xIn = clamp(APP->scene->getMousePos().x/APP->scene->box.size.x,0.f,1.f);
+      float yIn = 1-clamp(APP->scene->getMousePos().y/APP->scene->box.size.y,0.f,1.f);
+      if (mod->params[MousePad::TOG_PARAM].getValue()) { // toggle mode
+        if (activate && !activateState){
+          active = !active;
+          if (active) {
+            xRelativeOrigin = xIn;
+            yRelativeOrigin = yIn;
+          }
+        }
+      } else { // gate mode
+        active = activate;
+        if (active && !activateState){
+          xRelativeOrigin = xIn;
+          yRelativeOrigin = yIn;
+        }
+      }
+      activateState = activate;
+      if (!active){
+        mod->gate = 0.f;
+        if (mod->params[MousePad::XRETURN_PARAM].getValue()){
+          mod->x = 0.f;
+          xSave = 0.f;
+        } else xSave = mod->x;
+        if (mod->params[MousePad::YRETURN_PARAM].getValue()){
+          mod->y = 0.f;
+          ySave = 0.f;
+        } else ySave = mod->y;
+        return;
+      }
+      mod->gate = 10.f;
+      xIn -= mod->params[MousePad::XABS_PARAM].getValue() ? mod->params[MousePad::XORIGIN_PARAM].getValue() : xRelativeOrigin;
+      yIn -= mod->params[MousePad::YABS_PARAM].getValue() ? mod->params[MousePad::YORIGIN_PARAM].getValue() : yRelativeOrigin;
+      mod->x = xIn * mod->params[MousePad::XSCALE_PARAM].getValue() * 10.f + (mod->params[MousePad::XABS_PARAM].getValue() ? 0.f : xSave);
+      mod->y = yIn * mod->params[MousePad::YSCALE_PARAM].getValue() * 10.f + (mod->params[MousePad::YABS_PARAM].getValue() ? 0.f : ySave);
     }
-    VenomWidget::step();
   }
   
 };
