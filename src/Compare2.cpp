ef379e8928dd3bec7182c1c8e0a450d9696eede9
Dave Benham <david.benham42@gmail.com>
2025-07-21 22:32:46 -0400

    Add LEDs to Compare 2 outputs

--- src/Compare2.cpp
+++ src/Compare2.cpp
@@ -52,22 +52,25 @@ struct Compare2 : VenomModule {
     LSS_FF_OUTPUT,
     OUTPUTS_LEN
   };
-  enum LightId {
-//    ENUMS(HI_LIGHT,24)
-    LIGHTS_LEN
-  };
 
   using float_4 = simd::float_4;
   
   int oversample = 0;
   int oversampleValues[6]{1,2,4,8,16,32};
+  int monitor = 1;
+  int lightWait = 0;
   float_4 xorOld[4][3]{}, ffOld[4][3]{};
   OversampleFilter_4 upSample[4][INPUTS_LEN]{}, downSample[4][OUTPUTS_LEN]{};
   float outScale[6]{1.f,5.f,10.f,2.f,10.f,20.f},
         outOffset[6]{0.f,0.f,0.f,-1.f,-5.f,-10.f};
 
+  void configOut(int id, std::string label){
+    configOutput(id,label);
+    configLight(id,label+" indicator");
+  }
+
   Compare2() {
-    venomConfig(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+    venomConfig(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, OUTPUTS_LEN);
 
     configParam(SHIFT1_PARAM, -10.f, 10.f, 0.f, "Shift 1");
     configParam(SIZE1_PARAM, 0.f, 10.f, 5.f, "Size 1");
@@ -84,58 +87,61 @@ struct Compare2 : VenomModule {
     configInput(IN2_INPUT, "In 2");
     configSwitch<FixedSwitchQuantity>(OVER_PARAM, 0.f, 5.f, 0.f, "Oversample", {"Off", "x2", "x4", "x8", "x16", "x32"});
 
-    configOutput(GTR1_OUTPUT,"In 1 greater");
-    configOutput(NGTR1_OUTPUT,"In 1 not greater");
-    configOutput(NGTR2_OUTPUT,"In 2 not greater");
-    configOutput(GTR2_OUTPUT,"In 2 greater");
+    configOut(GTR1_OUTPUT,"In 1 greater");
+    configOut(NGTR1_OUTPUT,"In 1 not greater");
+    configOut(NGTR2_OUTPUT,"In 2 not greater");
+    configOut(GTR2_OUTPUT,"In 2 greater");
 
-    configOutput(EQ1_OUTPUT,"In 1 equal");
-    configOutput(NEQ1_OUTPUT,"In 1 not equal");
-    configOutput(NEQ2_OUTPUT,"In 2 not equal");
-    configOutput(EQ2_OUTPUT,"In 2 equal");
+    configOut(EQ1_OUTPUT,"In 1 equal");
+    configOut(NEQ1_OUTPUT,"In 1 not equal");
+    configOut(NEQ2_OUTPUT,"In 2 not equal");
+    configOut(EQ2_OUTPUT,"In 2 equal");
 
-    configOutput(LSS1_OUTPUT,"In 1 less");
-    configOutput(NLSS1_OUTPUT,"In 1 not less");
-    configOutput(NLSS2_OUTPUT,"In 2 not less");
-    configOutput(LSS2_OUTPUT,"In 2 less");
+    configOut(LSS1_OUTPUT,"In 1 less");
+    configOut(NLSS1_OUTPUT,"In 1 not less");
+    configOut(NLSS2_OUTPUT,"In 2 not less");
+    configOut(LSS2_OUTPUT,"In 2 less");
     
-    configOutput(GTR_AND_OUTPUT,"In 1 AND In 2 greater");
-    configOutput(GTR_OR_OUTPUT,"In 1 OR In 2 greater");
-    configOutput(GTR_XOR_OUTPUT,"In 1 XOR In 2 greater");
-    configOutput(GTR_FF_OUTPUT,"XOR greater flip flop");
+    configOut(GTR_AND_OUTPUT,"In 1 AND In 2 greater");
+    configOut(GTR_OR_OUTPUT,"In 1 OR In 2 greater");
+    configOut(GTR_XOR_OUTPUT,"In 1 XOR In 2 greater");
+    configOut(GTR_FF_OUTPUT,"XOR greater flip flop");
     
-    configOutput(EQ_AND_OUTPUT,"In 1 AND In 2 equal");
-    configOutput(EQ_OR_OUTPUT,"In 1 OR In 2 equal");
-    configOutput(EQ_XOR_OUTPUT,"In 1 XOR In 2 equal");
-    configOutput(EQ_FF_OUTPUT,"XOR equal flip flop");
+    configOut(EQ_AND_OUTPUT,"In 1 AND In 2 equal");
+    configOut(EQ_OR_OUTPUT,"In 1 OR In 2 equal");
+    configOut(EQ_XOR_OUTPUT,"In 1 XOR In 2 equal");
+    configOut(EQ_FF_OUTPUT,"XOR equal flip flop");
     
-    configOutput(LSS_AND_OUTPUT,"In 1 AND In 2 less");
-    configOutput(LSS_OR_OUTPUT,"In 1 OR In 2 less");
-    configOutput(LSS_XOR_OUTPUT,"In 1 XOR In 2 less");
-    configOutput(LSS_FF_OUTPUT,"XOR less flip flop");
+    configOut(LSS_AND_OUTPUT,"In 1 AND In 2 less");
+    configOut(LSS_OR_OUTPUT,"In 1 OR In 2 less");
+    configOut(LSS_XOR_OUTPUT,"In 1 XOR In 2 less");
+    configOut(LSS_FF_OUTPUT,"XOR less flip flop");
+
+    oversampleStages = 5;
+  }
+
+  void setOversample() override {
+    oversample = oversampleValues[static_cast<int>(params[OVER_PARAM].getValue())];
+    if (oversample > 1) {
+      for (int s=0; s<4; s++){
+        for (int i=0; i<INPUTS_LEN; i++)
+          upSample[s][i].setOversample(oversample, oversampleStages);
+        for (int i=0; i<OUTPUTS_LEN; i++)
+          downSample[s][i].setOversample(oversample, oversampleStages);
+      }
+    }
   }
   
   void process(const ProcessArgs& args) override {
     VenomModule::process(args);
     
-//    using float_4 = simd::float_4;
-
     bool hasIn[INPUTS_LEN]{}, hasOut[OUTPUTS_LEN]{};
-    float_4 in[INPUTS_LEN]{}, out[OUTPUTS_LEN]{};
+    float_4 in[INPUTS_LEN]{}, out[4][OUTPUTS_LEN]{}, out2[OUTPUTS_LEN]{};
     float_4 f4_0=float_4::zero(), f4_1=1.f, f4_2=2.f;
     
     // update oversample configuration
-    if (oversample != oversampleValues[static_cast<int>(params[OVER_PARAM].getValue())]) {
-      oversample = oversampleValues[static_cast<int>(params[OVER_PARAM].getValue())];
-      if (oversample > 1) {
-        for (int s=0; s<4; s++){
-          for (int i=0; i<INPUTS_LEN; i++)
-            upSample[s][i].setOversample(oversample, 5);
-          for (int i=0; i<OUTPUTS_LEN; i++)
-            downSample[s][i].setOversample(oversample, 5);
-        }
-      }
-    }
+    if (oversample != oversampleValues[static_cast<int>(params[OVER_PARAM].getValue())])
+      setOversample();
     // compute channel count
     int channels = 1;
     for (int i=0; i<INPUTS_LEN; i++)
@@ -177,64 +183,88 @@ struct Compare2 : VenomModule {
         float_4 hi2 = in[SHIFT2_INPUT] + params[SHIFT2_PARAM].getValue() + (in[SIZE2_INPUT] + params[SIZE2_PARAM].getValue())/2.f;
         float_4 lo2 = hi2 - in[SIZE2_INPUT] - params[SIZE2_PARAM].getValue();
         // compute compare outs
-        out[GTR1_OUTPUT] = ifelse(in[IN1_INPUT] > hi1, f4_1, f4_0);
-        out[LSS1_OUTPUT] = ifelse(in[IN1_INPUT] < lo1, f4_1, f4_0);
-        out[EQ1_OUTPUT] = ifelse(out[GTR1_OUTPUT]+out[LSS1_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NGTR1_OUTPUT] = ifelse(out[GTR1_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NEQ1_OUTPUT] = ifelse(out[EQ1_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NLSS1_OUTPUT] = ifelse(out[LSS1_OUTPUT] == f4_0, f4_1, f4_0);
-        out[GTR2_OUTPUT] = ifelse(in[IN2_INPUT] > hi2, f4_1, f4_0);
-        out[LSS2_OUTPUT] = ifelse(in[IN2_INPUT] < lo2, f4_1, f4_0);
-        out[EQ2_OUTPUT] = ifelse(out[GTR2_OUTPUT]+out[LSS2_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NGTR2_OUTPUT] = ifelse(out[GTR2_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NEQ2_OUTPUT] = ifelse(out[EQ2_OUTPUT] == f4_0, f4_1, f4_0);
-        out[NLSS2_OUTPUT] = ifelse(out[LSS2_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][GTR1_OUTPUT] = ifelse(in[IN1_INPUT] > hi1, f4_1, f4_0);
+        out[s][LSS1_OUTPUT] = ifelse(in[IN1_INPUT] < lo1, f4_1, f4_0);
+        out[s][EQ1_OUTPUT] = ifelse(out[s][GTR1_OUTPUT]+out[s][LSS1_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NGTR1_OUTPUT] = ifelse(out[s][GTR1_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NEQ1_OUTPUT] = ifelse(out[s][EQ1_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NLSS1_OUTPUT] = ifelse(out[s][LSS1_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][GTR2_OUTPUT] = ifelse(in[IN2_INPUT] > hi2, f4_1, f4_0);
+        out[s][LSS2_OUTPUT] = ifelse(in[IN2_INPUT] < lo2, f4_1, f4_0);
+        out[s][EQ2_OUTPUT] = ifelse(out[s][GTR2_OUTPUT]+out[s][LSS2_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NGTR2_OUTPUT] = ifelse(out[s][GTR2_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NEQ2_OUTPUT] = ifelse(out[s][EQ2_OUTPUT] == f4_0, f4_1, f4_0);
+        out[s][NLSS2_OUTPUT] = ifelse(out[s][LSS2_OUTPUT] == f4_0, f4_1, f4_0);
         // compute logic outs
-        out[GTR_AND_OUTPUT] = ifelse(out[GTR1_OUTPUT]+out[GTR2_OUTPUT] == f4_2, f4_1, f4_0);
-        out[GTR_OR_OUTPUT] = ifelse(out[GTR1_OUTPUT]+out[GTR2_OUTPUT] > f4_0, f4_1, f4_0);
-        out[GTR_XOR_OUTPUT] = ifelse(out[GTR1_OUTPUT] != out[GTR2_OUTPUT], f4_1, f4_0);
-        out[EQ_AND_OUTPUT] = ifelse(out[EQ1_OUTPUT]+out[EQ2_OUTPUT] == f4_2, f4_1, f4_0);
-        out[EQ_OR_OUTPUT] = ifelse(out[EQ1_OUTPUT]+out[EQ2_OUTPUT] > f4_0, f4_1, f4_0);
-        out[EQ_XOR_OUTPUT] = ifelse(out[EQ1_OUTPUT] != out[EQ2_OUTPUT], f4_1, f4_0);
-        out[LSS_AND_OUTPUT] = ifelse(out[LSS1_OUTPUT]+out[LSS2_OUTPUT] == f4_2, f4_1, f4_0);
-        out[LSS_OR_OUTPUT] = ifelse(out[LSS1_OUTPUT]+out[LSS2_OUTPUT] > f4_0, f4_1, f4_0);
-        out[LSS_XOR_OUTPUT] = ifelse(out[LSS1_OUTPUT] != out[LSS2_OUTPUT], f4_1, f4_0);
+        out[s][GTR_AND_OUTPUT] = ifelse(out[s][GTR1_OUTPUT]+out[s][GTR2_OUTPUT] == f4_2, f4_1, f4_0);
+        out[s][GTR_OR_OUTPUT] = ifelse(out[s][GTR1_OUTPUT]+out[s][GTR2_OUTPUT] > f4_0, f4_1, f4_0);
+        out[s][GTR_XOR_OUTPUT] = ifelse(out[s][GTR1_OUTPUT] != out[s][GTR2_OUTPUT], f4_1, f4_0);
+        out[s][EQ_AND_OUTPUT] = ifelse(out[s][EQ1_OUTPUT]+out[s][EQ2_OUTPUT] == f4_2, f4_1, f4_0);
+        out[s][EQ_OR_OUTPUT] = ifelse(out[s][EQ1_OUTPUT]+out[s][EQ2_OUTPUT] > f4_0, f4_1, f4_0);
+        out[s][EQ_XOR_OUTPUT] = ifelse(out[s][EQ1_OUTPUT] != out[s][EQ2_OUTPUT], f4_1, f4_0);
+        out[s][LSS_AND_OUTPUT] = ifelse(out[s][LSS1_OUTPUT]+out[s][LSS2_OUTPUT] == f4_2, f4_1, f4_0);
+        out[s][LSS_OR_OUTPUT] = ifelse(out[s][LSS1_OUTPUT]+out[s][LSS2_OUTPUT] > f4_0, f4_1, f4_0);
+        out[s][LSS_XOR_OUTPUT] = ifelse(out[s][LSS1_OUTPUT] != out[s][LSS2_OUTPUT], f4_1, f4_0);
         // compute flip flop outs
         for (int i=0; i<4; i++){
-          if (xorOld[s][0][i] != out[GTR_XOR_OUTPUT][i]){
-            xorOld[s][0][i] = out[GTR_XOR_OUTPUT][i];
-            if (out[GTR_XOR_OUTPUT][i])
+          if (xorOld[s][0][i] != out[s][GTR_XOR_OUTPUT][i]){
+            xorOld[s][0][i] = out[s][GTR_XOR_OUTPUT][i];
+            if (out[s][GTR_XOR_OUTPUT][i])
               ffOld[s][0][i] = !ffOld[s][0][i];
           }
-          if (xorOld[s][1][i] != out[EQ_XOR_OUTPUT][i]){
-            xorOld[s][1][i] = out[EQ_XOR_OUTPUT][i];
-            if (out[EQ_XOR_OUTPUT][i])
+          if (xorOld[s][1][i] != out[s][EQ_XOR_OUTPUT][i]){
+            xorOld[s][1][i] = out[s][EQ_XOR_OUTPUT][i];
+            if (out[s][EQ_XOR_OUTPUT][i])
               ffOld[s][1][i] = !ffOld[s][1][i];
           }
-          if (xorOld[s][2][i] != out[LSS_XOR_OUTPUT][i]){
-            xorOld[s][2][i] = out[LSS_XOR_OUTPUT][i];
-            if (out[LSS_XOR_OUTPUT][i])
+          if (xorOld[s][2][i] != out[s][LSS_XOR_OUTPUT][i]){
+            xorOld[s][2][i] = out[s][LSS_XOR_OUTPUT][i];
+            if (out[s][LSS_XOR_OUTPUT][i])
               ffOld[s][2][i] = !ffOld[s][2][i];
           }
         }
-        out[GTR_FF_OUTPUT] = ffOld[s][0];
-        out[EQ_FF_OUTPUT] = ffOld[s][1];
-        out[LSS_FF_OUTPUT] = ffOld[s][2];
-        // downsample outputs
-        if (oversample > 1){
-          for (int i=0; i<OUTPUTS_LEN; i++){
-            if (hasOut[i])
-              out[i] = downSample[s][i].process(out[i]);
-          }
+        out[s][GTR_FF_OUTPUT] = ffOld[s][0];
+        out[s][EQ_FF_OUTPUT] = ffOld[s][1];
+        out[s][LSS_FF_OUTPUT] = ffOld[s][2];
+        // downsample outputs if needed
+        for (int i=0; i<OUTPUTS_LEN; i++){
+          out2[i] = oversample>1 && hasOut[i] ? downSample[s][i].process(out[s][i]) : out[s][i];
         }
       } // end oversample loop
       // write outputs
       for (int i=0; i<OUTPUTS_LEN; i++)
-        outputs[i].setVoltageSimd(out[i]*scale+offset, c);
+        outputs[i].setVoltageSimd(out2[i]*scale+offset, c);
     } // end channel loop
     // set output channel counts
     for (int i=0; i<OUTPUTS_LEN; i++)
       outputs[i].setChannels(channels);
+    if (++lightWait>5) {
+      lightWait = 0;
+      int start = monitor<=1 ? 0 : monitor-2,
+          end = monitor==0 ? -1 : (monitor==1 ? channels : monitor - 1),
+          div = end - start;
+      if (end>channels)
+        end = -1;
+      for (int i=0; i<OUTPUTS_LEN; i++){
+        float val = 0;
+        for (int c=start; c<end; c++)
+          val += out[c/4][i][c%4];
+        lights[i].setBrightnessSmooth(val/div, args.sampleTime*5);
+      }
+    }
+  }
+  
+  json_t* dataToJson() override {
+    json_t* rootJ = VenomModule::dataToJson();
+    json_object_set_new(rootJ, "monitorChannel", json_integer(monitor));
+    return rootJ;
+  }
+
+  void dataFromJson(json_t* rootJ) override {
+    VenomModule::dataFromJson(rootJ);
+    json_t* val;
+    if ((val = json_object_get(rootJ, "monitorChannel")))
+      monitor = json_integer_value(val);
   }
 
 };
@@ -286,15 +316,28 @@ struct Compare2Widget : VenomWidget {
     // compare outputs
     for (float y=159.f; y<=221.f; y+=31.f){
       for (float x=14.f; x<=113.f; x+=33.f){
-        addOutput(createOutput<PolyPort>(Vec(x,y), module, id++));
+        addOutput(createOutput<PolyPort>(Vec(x,y), module, id));
+        addChild(createLight<SmallLight<YellowLight>>(Vec(x+21.f, y-2.f), module, id++));
       }
     }
     // logic outputs
     for (float y=264.f; y<=326.f; y+=31.f){
       for (float x=14.f; x<=113.f; x+=33.f){
-        addOutput(createOutput<PolyPort>(Vec(x,y), module, id++));
+        addOutput(createOutput<PolyPort>(Vec(x,y), module, id));
+        addChild(createLight<SmallLight<YellowLight>>(Vec(x+21.f, y-2.f), module, id++));
       }
     }
+  }
+
+  void appendContextMenu(Menu* menu) override {
+    Compare2* module = static_cast<Compare2*>(this->module);
+    menu->addChild(new MenuSeparator);
+    menu->addChild(createIndexPtrSubmenuItem(
+      "Monitor channel",
+      {"Off","All","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"},
+      &module->monitor
+    ));
+    VenomWidget::appendContextMenu(menu);
   }
 
 };
